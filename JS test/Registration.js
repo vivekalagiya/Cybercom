cd597eccb16e39478e96213346b85f92c6f084c0
Vivek Alagiya <vivekalagiya111@gmail.com>
2021-02-06 14:59:38 +0530

    4 hour commit

---
+++ JS test/Registration.js
@@ -0,0 +1,56 @@
+const form = document.getElementById('form');
+console.log('chceck...');
+
+
+var arrAdmin = [];
+var flag = 0;
+
+function addElement() {
+
+    console.log('check');
+    var fname = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var pwd = document.getElementById('password').value;
+    var cpwd = document.getElementById('cpassword').value;
+    var city = document.getElementById('city').value;
+    var state = document.getElementById('state').value;
+
+
+    var admin = {
+        name: fname,
+        email: email,
+        password: pwd,
+        city: city,
+        state: state
+    };
+
+    if (localStorage.getItem('arrAdmin')) {
+        arrAdmin = JSON.parse(localStorage.getItem('arrAdmin'));
+        console.log(arrAdmin);
+    }
+
+    function admin_exist() {
+        for (var index = 0; index < arrAdmin.length; ++index) {
+
+            var temp = arrAdmin[index];
+
+            if (temp.email == email) {
+                flag = 1;
+                alert("admin already exist!");
+                break;
+            }
+        }
+    }
+    admin_exist();
+    if (flag === 0) {
+        arrAdmin.push(admin);
+        console.log(arrAdmin);
+        localStorage.setItem("arrAdmin", JSON.stringify(arrAdmin));
+        var message = window.confirm("registerd successfully");
+        if (message) {
+            window.location.href = "login.html";
+        }
+    }
+
+};
+
